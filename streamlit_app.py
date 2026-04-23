e91f63bc4bd0acd9b0285676d6d4c652913a7609
Scott Snelgrove <scott.snelgrove@gomerits.com>
2026-04-23 10:18:18 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,44 @@
-# Import python packages.
+# Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col, when_matched
 
-# Write directly to the app.
-st.title(":cup_with_straw:Pending Smoothie Orders:cup_with_straw:")
-st.write("""Orders that need to be filled.""")
+# Write directly to the app
+st.title(f"Example Streamlit App :balloon: {st.__version__}")
+st.write(
+  """Replace this example with your own code!
+  **And if you're new to Streamlit,** check
+  out our easy-to-follow guides at
+  [docs.streamlit.io](https://docs.streamlit.io).
+  """
+)
 
-
-
+# Get the current credentials
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.orders") \
-    .filter(col("ORDER_FILLED") == 0) \
-    .collect()
 
-if my_dataframe:
-    editable_df = st.data_editor(my_dataframe)
-    submitted = st.button('Submit')
-    
-    if submitted:
-        og_dataset = session.table("smoothies.public.orders")
-        edited_dataset = session.create_dataframe(editable_df)
-        
-        try:
-            og_dataset.merge(edited_dataset
-                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                        )
-            st.success('Someone clicked the button.', icon="👍")
-        except:
-            st.write('Smoething went wrong')
-else:
-    st.success('There are no pending orders right now', icon="👍")
+# Use an interactive slider to get user input
+hifives_val = st.slider(
+  "Number of high-fives in Q3",
+  min_value=0,
+  max_value=90,
+  value=60,
+  help="Use this to enter the number of high-fives you gave in Q3",
+)
+
+#  Create an example dataframe
+#  Note: this is just some dummy data, but you can easily connect to your Snowflake data
+#  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
+created_dataframe = session.create_dataframe(
+  [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
+  schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
+)
+
+# Execute the query and convert it into a Pandas dataframe
+queried_data = created_dataframe.to_pandas()
+
+# Create a simple bar chart
+# See docs.streamlit.io for more types of charts
+st.subheader("Number of high-fives")
+st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
+
+st.subheader("Underlying data")
+st.dataframe(queried_data, use_container_width=True)
